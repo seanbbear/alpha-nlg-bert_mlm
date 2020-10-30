35c2d55230ac60d1fa7777af48927cd9b90d5b57
seanbbear <k7489759@gmail.com>
2020-10-30 10:18:12 +0000

    first commit

---
+++ data_preprocess.py
@@ -0,0 +1,124 @@
+import jsonlines
+from transformers import AutoTokenizer
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset
+
+def get_dataset(path):
+    # obs1 + obs2 + hyp1 + hyp2 在 train 的最大長度為448 在 dev 的為329
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased") 
+
+    with jsonlines.open(path) as f:
+        data_len = 0
+        for obj in f:
+            hyp = obj['hyp'] + '[SEP]'
+            data_len += len(tokenizer.tokenize(hyp))
+        print(data_len)
+
+    with jsonlines.open(path) as f:
+
+        input_ids = np.zeros(shape=(data_len,512))     
+        token_type_ids = np.zeros(shape=(data_len,512))     
+        attention_mask = np.zeros(shape=(data_len,512))  
+        answer = np.zeros(shape=(data_len,512))  
+        
+        index = 0
+        for obj in f:
+
+            obs1 = '[CLS]' + obj['obs1'] + '[SEP]'
+            obs2 = obj['obs2'] + '[SEP]'
+            hyp = obj['hyp'] + '[SEP]'
+
+            obs1_word_piece = tokenizer.tokenize(obs1)
+            obs2_word_piece = tokenizer.tokenize(obs2)
+            hyp_word_piece = tokenizer.tokenize(hyp)
+
+            tensor_list = input_feature(tokenizer, obs1_word_piece, obs2_word_piece, hyp_word_piece)
+            for tensor in tensor_list:
+                # print(tensor['input_ids'])
+                input_ids[index] = tensor['input_ids']
+                token_type_ids[index] = tensor['token_type_ids']
+                attention_mask[index] = tensor['attention_mask']     
+                answer[index] = tensor['masked_lm_labels']
+                # print(index)
+                index += 1
+            # break
+
+        input_ids = torch.LongTensor(input_ids)
+        token_type_ids = torch.LongTensor(token_type_ids)
+        attention_mask = torch.LongTensor(attention_mask)     
+        answer = torch.LongTensor(answer)
+        print(input_ids.shape, token_type_ids.shape, attention_mask.shape, answer.shape)
+    return TensorDataset(input_ids, token_type_ids, attention_mask, answer)
+
+def input_feature(tokenizer, obs1, obs2, hyp):
+    tensor_list = []
+
+    max_length = 512
+    obs1_ids = tokenizer.convert_tokens_to_ids(obs1)
+    obs2_ids = tokenizer.convert_tokens_to_ids(obs2)
+    hyp_ids = tokenizer.convert_tokens_to_ids(hyp)
+
+    input_ids  = []
+    token_type_ids  = []
+    attention_mask = []
+    masked_lm_labels = []
+
+    # 先append第一句 token_type為0
+    for i in range(len(obs1_ids)):
+        input_ids.append(obs1_ids[i])
+        token_type_ids.append(0)
+        attention_mask.append(1)
+        masked_lm_labels.append(-100)
+
+    # append第二句 token_type為1
+    for i in range(len(obs2_ids)):
+        input_ids.append(obs2_ids[i])
+        token_type_ids.append(1)
+        attention_mask.append(1)
+        masked_lm_labels.append(-100)
+
+    # append answer
+    # [MASK]的id為103
+    for i in range(len(hyp_ids)):
+        input_ids.append(hyp_ids[i])
+        token_type_ids.append(0)
+        attention_mask.append(1)
+        masked_lm_labels.append(-100)
+
+        tensor_list.append(full_tensor(input_ids, token_type_ids, attention_mask, masked_lm_labels, max_length))
+
+        
+    return tensor_list
+
+def full_tensor(input_ids, token_type_ids, attention_mask, masked_lm_labels, max_length):
+    new_input_ids = input_ids[:]
+    new_token_type_ids = token_type_ids[:]
+    new_attention_mask = attention_mask[:]
+    new_masked_lm_labels = masked_lm_labels[:]
+
+    temp = new_input_ids[-1]
+    new_input_ids[-1] = 103 # 把最後一個input_ids改成[MASK]
+    new_masked_lm_labels[-1] = temp # 把最後一個[MASK]改成要預測的id
+    
+    while len(new_input_ids) < max_length:
+        new_input_ids.append(0)
+        new_token_type_ids.append(0)
+        new_attention_mask.append(0)
+        new_masked_lm_labels.append(-100)
+
+    
+    assert len(new_input_ids) == len(new_token_type_ids) == len(new_attention_mask) == len(new_masked_lm_labels) == 512
+    
+    new_features = {'input_ids':new_input_ids,
+                    'token_type_ids':new_token_type_ids,
+                    'attention_mask':new_attention_mask,
+                    'masked_lm_labels':new_masked_lm_labels}
+    return new_features
+     
+
+
+if __name__=="__main__":
+    # get_dataset('../../data/unique/train-unique_obs1_obs2.jsonl')
+    get_dataset('../../data/unique/short.jsonl')
---
+++ main.py
@@ -0,0 +1,106 @@
+from data_preprocess import get_dataset
+from torch.utils.data import DataLoader
+from transformers import BertConfig, BertForMaskedLM, AdamW
+import os
+import torch
+
+import wandb
+
+if __name__=="__main__":
+    # wandb專案名稱
+    wandb.init(project="alpha-nlg")
+
+    # setting device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    
+    batch_size = 4
+    lr = 5e-6
+    training_epoch = 6
+    
+    # 
+    config = BertConfig.from_pretrained('bert-base-uncased')
+    model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+
+    # 多GPU
+    if  torch.cuda.device_count()>1:         
+        model = torch.nn.DataParallel(model,device_ids=[0,1])
+
+    model.to(device)
+
+    wandb.watch(model)
+
+    train_dataset = get_dataset('../../data/unique/train-unique_obs1_obs2.jsonl')
+    dev_dataset = get_dataset('../../data/unique/dev-unique_obs1_obs2.jsonl')
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=True)
+
+    # Prepare optimizer and schedule (linear warmup and decay)
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.1},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.1}
+        ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=lr, eps=1e-8)
+
+    model.zero_grad()
+    for epoch in range(training_epoch):
+        train_loss = 0.0
+        train_acc = 0.0
+        model.train()
+        for batch_index, batch_dict in enumerate(train_dataloader):
+            batch_dict = tuple(t.to(device) for t in batch_dict)
+            
+            outputs = model(
+                input_ids = batch_dict[0],
+                token_type_ids = batch_dict[1],
+                attention_mask = batch_dict[2],
+                masked_lm_labels=batch_dict[3]
+            )
+            loss, logits = outputs[:2]
+            
+            if (device=='cuda' and device,torch.cuda.device_count()>1):
+                loss = loss.mean()
+
+            loss.backward()
+            optimizer.step()
+            model.zero_grad()
+
+            # 計算loss
+            loss_t = loss.item()
+            train_loss += (loss_t - train_loss)/(batch_index + 1)
+
+        # log
+        print("epoch:%2d batch:%4d train_loss:%2.4f "%(epoch+1, batch_index+1, train_loss))
+        wandb.log({"Train Loss":train_loss})
+
+        test_loss = 0.0
+        test_acc = 0.0
+        model.eval()
+        for batch_index, batch_dict in enumerate(dev_dataloader):
+            batch_dict = tuple(t.to(device) for t in batch_dict)
+            
+            outputs = model(
+                input_ids = batch_dict[0],
+                token_type_ids = batch_dict[1],
+                attention_mask = batch_dict[2],
+                masked_lm_labels=batch_dict[3]
+            )
+            loss,logits = outputs[:2]
+
+            if (device=='cuda' and device,torch.cuda.device_count()>1):                 
+                loss = loss.mean()
+            
+            
+            # 計算loss
+            loss_t = loss.item()
+            test_loss += (loss_t - test_loss) / (batch_index + 1)
+
+            # log
+        print("epoch:%2d batch:%4d test_loss:%2.4f "%(epoch+1, batch_index+1, test_loss))
+        wandb.log({"Test Loss":test_loss})
+        
+        torch.save(model.state_dict(), os.path.join(wandb.run.dir, 'model.pt'))
+
+        model_to_save = model.module if hasattr(model, 'module') else model
+        model_to_save.save_pretrained('trained_model/' + str(epoch))
